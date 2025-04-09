099f111e0985f041fff531d17a28a6552837d11b
Davin <30064993+D-Cach@users.noreply.github.com>
2025-04-08 18:59:48 -0700

    Commit for project 7

--- main.py
+++ main.py
@@ -14,44 +14,32 @@ class TestUrbanRoutes:
 
     # Add in S8
     def test_set_route(self):
-
         print("function created for set route")
-
         pass
 
     # Add in S8
     def test_select_plan(self):
-
         print("function created for select plan")
-
         pass
 
     # Add in S8
     def test_fill_phone_number(self):
-
         print("function created for fill phone number")
-
         pass
 
     # Add in S8
     def test_fill_card(self):
-
         print("function created for fill card")
-
         pass
 
     # Add in S8
     def test_comment_for_driver(self):
-
         print("function created for comment for driver")
-
         pass
 
     # Add in S8
     def test_order_blanket_and_handkerchiefs(self):
-
         print("function created for order blanket and handkerchiefs")
-
         pass
 
     # Add in S8
@@ -59,13 +47,10 @@ class TestUrbanRoutes:
         for i in range(2):
             # Add in S8
             pass
-
         print("function created for order 2 ice creams")
         pass
 
     # Add in S8
     def test_car_search_model_appears(self):
-
         print("function created for car search model appears")
-
         pass
